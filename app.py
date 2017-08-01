23d2994d1ea4b65018f1d722e94df77322eb8c71
Will Carpenter <willcarpenteriii@gmail.com>
2017-08-01 11:13:54 -0600

    can post new song to db

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 import sqlite3
 from flask import Flask, request, jsonify
 
-app = Flask(__name__)
+import json
 
-if __name__ == '__main__':
-	app.debug = True
-	app.run()
+app = Flask(__name__)
 
 
 @app.route('/api/songs', methods=['GET', 'POST'])
 def collection():
 	if request.method == 'GET':
-		pass  # Handle GET all Request
+		all_songs = get_all_songs()
+		return json.dumps(all_songs)
 	elif request.method == 'POST':
 		data = request.form
 		result = add_song(data['artist'], data['title'], data['rating'])
@@ -39,3 +38,16 @@ def add_song(artist, title, rating):
 	except:
 		result = {'status': 0, 'message': 'error'}
 	return result
+
+
+def get_all_songs():
+	with sqlite3.connect('songs.db') as connection:
+		cursor = connection.cursor()
+		cursor.execute("SELECT * FROM songs ORDER BY id DESC")
+		all_songs = cursor.fetchall()
+		return all_songs
+
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run()
